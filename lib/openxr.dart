f98eb6ae4d63bd7c7f2d234b15fbd7cce594102d
Arto Bendiken <arto@bendiken.net>
2020-05-11 19:56:57 +0300

    Sketched out the library structure.

--- lib/openxr.dart
+++ lib/openxr.dart
@@ -1,3 +1,19 @@
 /* This is free and unencumbered software released into the public domain. */
 
 library openxr;
+
+export 'src/abi.dart';
+export 'src/api.dart';
+
+export 'src/action.dart';
+export 'src/action_set.dart';
+export 'src/extension.dart';
+export 'src/handle.dart';
+export 'src/instance.dart';
+export 'src/layer.dart';
+export 'src/result.dart';
+export 'src/session.dart';
+export 'src/space.dart';
+export 'src/swapchain.dart';
+export 'src/system.dart';
+export 'src/version.dart';
